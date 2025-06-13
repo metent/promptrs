9868ec0f80ffa178a57b63ff45799c17e4ec32b5
Rishabh Das <rdas@tutanota.com>
2025-06-13 09:02:33 +0530

    use esbuild for bundling before transpiling

--- examples/org-agent/componentize.sh
+++ examples/org-agent/componentize.sh
@@ -2,23 +2,29 @@ swc \
   -C jsc.parser.syntax=typescript \
   -C jsc.parser.decorators=true \
   -C jsc.transform.decoratorVersion='2022-03' \
-  -C "jsc.baseUrl=\"$(pwd)\"" \
-  -C 'jsc.paths.node:fs=["./shim/fs.ts"]' \
-  -C 'jsc.paths.fs=["./shim/fs.ts"]' \
   -C env.targets.node=23 \
   -d dist \
   --out-file-extension ts \
-  src shim \
+  src \
 && bun build \
   --target node \
-  -e wasi:clocks/monotonic-clock@0.2.5 \
   -e wasi:filesystem/preopens@0.2.5 \
   -e wasi:filesystem/types@0.2.5 \
+  -e wasi:clocks/monotonic-clock@0.2.5 \
   --outfile dist/bundle.js \
-  dist/src/lib.js \
+  dist/src/lib.ts \
+&& esbuild \
+  --bundle \
+  '--external:wasi*' \
+  --alias:node:fs=./shim/fs.ts \
+  --alias:fs=./shim/fs.ts \
+  --platform=node \
+  --format=esm \
+  --outfile=dist/org_agent.js \
+  dist/bundle.js \
 && jco componentize \
   --wit wit/ \
   --world-name ifc \
   --disable http \
   --out dist/org_agent.wasm \
-  dist/bundle.js
+  dist/org_agent.js
